b4a875f82cae32232ed6b916ef1b26bcd83ac5d0
Pascal Hartig <passy@twitter.com>
2014-09-20 13:49:12 +0100

    Update to new yeoman-generator syntax

--- app/index.js
+++ app/index.js
@@ -1,46 +1,42 @@
 'use strict';
 
 var path = require('path');
-var util = require('util');
 var yeoman = require('yeoman-generator');
 
-function BookmarkletGenerator(args, options) {
-  yeoman.generators.Base.apply(this, arguments);
+module.exports = yeoman.generators.Base.extend({
+  constructor: function constructor() {
+    yeoman.generators.Base.apply(this, arguments);
 
-  this.mainFile = this.readFileAsString(path.join(this.sourceRoot(),
-                                                  'main.js'));
-  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname,
-                                                        '../package.json')));
+    this.mainFile = this.readFileAsString(
+      path.join(this.sourceRoot(), 'main.js'));
+    this.pkg = JSON.parse(
+      this.readFileAsString(path.join(__dirname, '../package.json')));
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.npmInstall();
+      }
+    });
+  },
 
-  this.on('end', function () {
-    if (!options['skip-install']) {
-      this.npmInstall();
-    }
-  });
-}
+  buildfile: function buildfile() {
+    this.template('gulpfile.js');
+  },
 
-util.inherits(BookmarkletGenerator, yeoman.generators.Base);
+  packagejson: function packagejson() {
+    this.template('_package.json', 'package.json');
+  },
 
-BookmarkletGenerator.prototype.buildfile = function buildfile() {
-  this.template('gulpfile.js');
-};
+  jshintrc: function jshintrc() {
+    this.template('jshintrc', '.jshintrc');
+  },
 
-BookmarkletGenerator.prototype.packagejson = function packagejson() {
-  this.template('_package.json', 'package.json');
-};
+  gitfiles: function gitfiles() {
+    this.copy('gitattributes', '.gitattributes');
+    this.copy('gitignore', '.gitignore');
+  },
 
-BookmarkletGenerator.prototype.jshintrc = function jshintrc() {
-  this.template('jshintrc', '.jshintrc');
-};
-
-BookmarkletGenerator.prototype.gitfiles = function gitfiles() {
-  this.copy('gitattributes', '.gitattributes');
-  this.copy('gitignore', '.gitignore');
-};
-
-BookmarkletGenerator.prototype.app = function app() {
-  this.mkdir('app');
-  this.write('app/main.js', this.mainFile);
-};
-
-module.exports = BookmarkletGenerator;
+  app: function app() {
+    this.mkdir('app');
+    this.write('app/main.js', this.mainFile);
+  }
+});
