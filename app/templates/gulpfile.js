08f2b48c7d08227cf37ed2d1ddb5edfd3af9503f
HaNdTriX <handtrix@gmail.com>
2014-10-04 15:25:18 +0200

    Update to Gulp Version 3.8

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -1,43 +1,36 @@
 // Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
-
 'use strict';
 
 var buffer = require('buffer');
 var gulp = require('gulp');
-var gulpClean = require('gulp-clean');
+var rimraf = require('gulp-rimraf');
 var gulpConcat = require('gulp-concat');
 var gulpJshint = require('gulp-jshint');
 var gulpUglify = require('gulp-uglify');
 var jshintStylish = require('jshint-stylish');
 var map = require('map-stream');
 
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
   var header = new Buffer('// Copy this to your URL bar:\njavascript:');
 
   gulp.src('app/{,*/}*.js')
     .pipe(gulpJshint())
     .pipe(gulpJshint.reporter(jshintStylish))
-    .pipe(gulpUglify({
-      compress: { 'negate_iife': false }
-    }))
+    .pipe(gulpUglify())
     .pipe(gulpConcat('bookmarklet.js'))
-    .pipe(map(function (file, cb) {
+    .pipe(map(function(file, cb) {
       file.contents = buffer.Buffer.concat([header, file.contents]);
       cb(null, file);
     }))
     .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('clean', function () {
-  gulp.src('dist').pipe(gulpClean());
+gulp.task('clean', function() {
+  gulp.src('dist').pipe(rimraf());
 });
 
-gulp.task('default', function () {
-  gulp.run('clean', 'scripts');
-});
+gulp.task('default', ['clean', 'scripts']);
 
-gulp.task('watch', function () {
-  gulp.watch('app/{,*/}*.js', function () {
-    gulp.run('scripts');
-  });
+gulp.task('watch', function() {
+  gulp.watch('app/{,*/}*.js', ['scripts']);
 });
